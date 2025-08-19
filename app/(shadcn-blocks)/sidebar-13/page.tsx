5ee3b465849789c41a1af9aa238c7805ab27b884
Wheza <wheza99@gmail.com>
2025-08-19 11:00:33 +0700

    delete unused sidebar

--- app/(shadcn-blocks)/sidebar-13/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SettingsDialog } from "@/components/shadcn-blocks/sidebar-13/settings-dialog"
-
-export default function Page() {
-  return (
-    <div className="flex h-svh items-center justify-center">
-      <SettingsDialog />
-    </div>
-  )
-}
